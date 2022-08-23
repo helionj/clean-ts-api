b1de2272ec1ff8ded00ce3ea4282c1a9e6a8835f
helionj <helionporto@hotmail.com>
2022-08-23 16:30:48 -0400

    feat: ensure cors is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyParser } from './middleware/boddy-parser'
+import { cors } from './middleware/cors'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
