712dafec81fbcabb22b12de8f9ab7cb86f75da0f
helionj <helionporto@hotmail.com>
2022-08-23 17:12:05 -0400

    feat: ensure requests return default content-type as json

---
+++ src/main/config/middleware/content-type.test.ts
@@ -0,0 +1,22 @@
+import app from '../app'
+import request from 'supertest'
+
+describe('Content Type Middleware', () => {
+  test('Should default content type as json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+  test('Should RETURN content type as XML when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParser } from './middleware/boddy-parser'
+import { contenType } from './middleware/content-type'
 import { cors } from './middleware/cors'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contenType)
 }
