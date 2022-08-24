8e10df6541b97969cebe3804096c9105a4485323
helionj <helionporto@hotmail.com>
2022-08-24 10:11:27 -0400

    feat: ensure SugnUp routes return an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import setUpMiddleweres from './middlewares'
-
+import setUpRoutes from './routes'
 const app = express()
 setUpMiddleweres(app)
+setUpRoutes(app)
 export default app
