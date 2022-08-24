4e294e8755a652967fac3ff71e8dd4b588ed5722
helionj <helionporto@hotmail.com>
2022-08-24 16:55:56 -0400

    test: ensure mongodb reconnect when connection is down

--- src/infra/criptography/db/mongodb/account-repository/account.spec.ts
+++ src/infra/criptography/db/mongodb/account-repository/account.spec.ts
@@ -14,7 +14,7 @@ describe('Mongo repository', () => {
     await MongoHelper.disconnect()
   })
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
