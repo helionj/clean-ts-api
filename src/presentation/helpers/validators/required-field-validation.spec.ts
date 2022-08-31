8260ed0d7dcb47a89c42c26f5a9fa2d060809230
helionj <helionporto@hotmail.com>
2022-08-31 11:06:47 -0400

    test: ensure RequiredValidationFields returns undefined if validation not fails

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -4,7 +4,12 @@ import { RequiredFieldValidation } from './required-field-validation'
 describe('Required Field Validation', () => {
   test('Should returns MissingParamError if validation fails', () => {
     const sut = new RequiredFieldValidation('field')
-    const error = sut.validate({ name: 'bad-field' })
+    const error = sut.validate({ name: 'any-name' })
     expect(error).toEqual(new MissingParamError('field'))
+  })
+  test('Should not returns if validation not fails', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any-name' })
+    expect(error).toBeFalsy()
   })
 })
