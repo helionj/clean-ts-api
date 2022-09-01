1e5961d44e12d7936aecd3a88922c9b093ee1dd8
helionj <helionporto@hotmail.com>
2022-09-01 08:45:38 -0400

    refactor: move HttpHelper to a folder

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,7 +1,7 @@
 import { AddAccount, AddAccountModel, AccountModel, HttpRequest, Validation } from './signup-protocols'
 import { MissingParamError, ServerError } from '../../errors'
 import { SignupController } from './signup'
-import { badRequest, ok, serverError } from '../../helpers/http-helper'
+import { badRequest, ok, serverError } from '../../helpers/http/http-helper'
 
 interface SutTypes {
   sut: SignupController
