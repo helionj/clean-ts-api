9843e42dbebb77f14e2a7831ffe462a6a963953b
helionj <helionporto@hotmail.com>
2022-09-02 15:55:48 -0400

    refactor: move folder db to infra

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,6 +1,6 @@
-import { AddAccountRepository } from '../../../../../data/protocols/db/add-account-repository'
-import { AccountModel } from '../../../../../domain/models/account'
-import { AddAccountModel } from '../../../../../domain/use-cases/add-account'
+import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/use-cases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
