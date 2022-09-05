2a648f4c428bb731782e9ae8bfe83b618edfb254
helionj <helionporto@hotmail.com>
2022-09-05 09:43:35 -0400

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://docker:mongopw@localhost:55000/clean-node-api?authSource=admin',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || '$%675849H&*'
 }
