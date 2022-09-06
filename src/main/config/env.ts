9f6d2812f8254a4d1487cba6c0596a24ceb177a4
helionj <helionporto@hotmail.com>
2022-09-05 22:19:45 -0400

    chore: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://docker:mongopw@localhost:55000/clean-node-api?authSource=admin',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://docker:mongopw@mongo:55000/clean-node-api?authSource=admin',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || '$%675849H&*'
 }
