a729f2b33adb97c9aa1df47da2082e2dd9f65947
helionj <helionporto@hotmail.com>
2022-09-06 10:00:17 -0400

    chore: add script user docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://docker:mongopw@mongo:55000/clean-node-api?authSource=admin',
+  /* mongoUrl: process.env.MONGO_URL || 'mongodb://docker:mongopw@mongo:55000/clean-node-api?authSource=admin', */
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api?authSource=admin',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || '$%675849H&*'
 }
