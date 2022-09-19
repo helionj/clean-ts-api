29f545eb76c103a5b7f44af5074566bce2e69f6b
helionj <helionporto@hotmail.com>
2022-09-19 14:48:42 -0400

    test: ensure add survey route returns 204 when accessToken valid is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import app from '../config/app'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -15,12 +18,43 @@ describe('Survey Routes', () => {
   })
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await surveyCollection.deleteMany({})
   })
   describe('POST /surveys', () => {
     test('Should return 403 on add survey without a token', async () => {
       await request(app)
         .post('/api/surveys')
+        .send({
+          question: 'Question 1',
+          answers: [
+            {
+              image: 'http://image-bank/image1.jpg',
+              answer: 'Answer 1'
+            },
+            {
+              answer: 'Answer 2'
+            }
+          ]
+        })
+        .expect(403)
+    })
+    test('Should return 204 on add survey with a valic accessToken', async () => {
+      const result = await accountCollection.insertOne({
+        name: 'Hélion Porto',
+        email: 'helionporto@gmail.com',
+        password: '123',
+        role: 'admin'
+      })
+      const { insertedId: id } = result
+      const accessToken = sign({ insertedId: id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, { $set: { accessToken } })
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-accessToken', accessToken)
         .send({
           question: 'Question 1',
           answers: [
