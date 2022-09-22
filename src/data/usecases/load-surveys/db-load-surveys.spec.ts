d2ba8a326ae9fb6a4c0429d4a3ce5fb1c99630bd
helionj <helionporto@hotmail.com>
2022-09-22 14:18:58 -0400

    test: ensure DbLoadSurvey return survey list on success

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -1,6 +1,33 @@
 import { SurveyModel, LoadSurveysRepository } from './db-load-surveys-protocols'
 import { DbLoadSurveys } from './db-load-surveys'
+import MockDate from 'mockdate'
 
+const makeFakeSurveys = (): SurveyModel[] => {
+  return [
+    {
+      id: 'any-id',
+      question: 'any-question',
+      answers: [
+        {
+          image: 'any-image',
+          answer: 'any-answer'
+        }
+      ],
+      date: new Date()
+    },
+    {
+      id: 'other-id',
+      question: 'other-question',
+      answers: [
+        {
+          image: 'other-image',
+          answer: 'other-answer'
+        }
+      ],
+      date: new Date()
+    }
+  ]
+}
 interface SutTypes {
   sut: DbLoadSurveys
   loadSurveysRepositoryStub: LoadSurveysRepository
@@ -9,7 +36,7 @@ interface SutTypes {
 const makeLoadSurveysRepositoryStub = (): LoadSurveysRepository => {
   class LoadSurveysRepositoryStub implements LoadSurveysRepository {
     async load (): Promise<SurveyModel[]> {
-      return await new Promise(resolve => resolve([]))
+      return await new Promise(resolve => resolve(makeFakeSurveys()))
     }
   }
   return new LoadSurveysRepositoryStub()
@@ -24,10 +51,22 @@ const makeSut = (): SutTypes => {
   }
 }
 describe('DbLoadSurveys', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+  afterAll(() => {
+    MockDate.reset()
+  })
   test('Should call LoadSurveysRepository with correct values', async () => {
     const { sut, loadSurveysRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadSurveysRepositoryStub, 'load')
     await sut.load()
     expect(loadSpy).toHaveBeenCalled()
+  })
+
+  test('Should return a survey list on success', async () => {
+    const { sut } = makeSut()
+    const response = await sut.load()
+    expect(response).toEqual(makeFakeSurveys())
   })
 })
