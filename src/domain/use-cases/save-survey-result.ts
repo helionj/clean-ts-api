6453bcdeea367c06f6de85bc23a1aa480d7fc959
helionj <helionporto@hotmail.com>
2022-09-27 09:29:44 -0400

    feat: add SaveSurveyResult usecase

---
+++ src/domain/use-cases/save-survey-result.ts
@@ -0,0 +1,8 @@
+import { SurveyResultModel } from '../models/survey-result'
+
+export type SaveSurveyResultModel = Omit<SurveyResultModel, 'id'>
+
+export interface SaveSurveyResult {
+
+  save: (data: SaveSurveyResultModel) => Promise<SurveyResultModel>
+}
