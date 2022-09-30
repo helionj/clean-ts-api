14ebe4f61a306c7363d10a820ca2d64fd2561982
helionj <helionporto@hotmail.com>
2022-09-30 14:07:46 -0400

    test: ensure SaveSurveyResultModel updates a surveyResult if it exist

--- src/infra/db/mongodb/survey/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-result/survey-result-mongo-repository.spec.ts
@@ -16,6 +16,10 @@ const makeSurvey = async (): Promise<SurveyModel> => {
         {
           image: 'any-image',
           answer: 'any-answer'
+        },
+        {
+          image: 'other-image',
+          answer: 'other-answer'
         }
       ],
       date: new Date()
@@ -74,5 +78,28 @@ describe('Survey Mongo repository', () => {
       expect(surveyResult.id).toBeTruthy()
       expect(surveyResult.answer).toBe(survey.answers[0].answer)
     })
+  })
+  test('Should update a resultSurvey if its not new', async () => {
+    const account = await makeAccount()
+    const survey = await makeSurvey()
+    const res = await surveyResultCollection.insertOne({
+      surveyId: survey.id,
+      accountId: account.id,
+      answer: survey.answers[0].answer,
+      date: new Date()
+    })
+    const sut = makeSut()
+    const surveyResult = await sut.save(
+      {
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[1].answer,
+        date: new Date()
+      }
+    )
+
+    expect(surveyResult).toBeTruthy()
+    expect(surveyResult.id).toEqual(res.insertedId.toHexString())
+    expect(surveyResult.answer).toBe(survey.answers[1].answer)
   })
 })
