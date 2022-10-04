89ad0d5c21bb091a35c38af29d1585933ca24292
helionj <helionporto@hotmail.com>
2022-10-04 10:48:54 -0400

    ensusre save survey result route returns 403 if no token provide

---
+++ src/main/factories/usecases/survey-result/save-survey-result/db-save-survey-result-factory.ts
@@ -0,0 +1,8 @@
+import { DbSaveSurveyResult } from '../../../../../data/usecases/survey-result/save-survey-result/db-save-survey-result'
+import { SaveSurveyResult } from '../../../../../domain/use-cases/survey-result/save-survey-result'
+import { SurveyResultMongoRepository } from '../../../../../infra/db/mongodb/survey/survey-result/survey-result-mongo-repository'
+
+export const makeDbSaveSurveyResult = (): SaveSurveyResult => {
+  const surveyResultMongoRepository = new SurveyResultMongoRepository()
+  return new DbSaveSurveyResult(surveyResultMongoRepository)
+}
